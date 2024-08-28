4737d02e505cc259e420fea765cd4c331ecc3de9
Leudo VM <leudo@ubuntu.com>
2024-08-28 18:13:38 -0300

    fix/celery app not being imported

--- weather_api_wrapper/celery.py
+++ weather_api_wrapper/celery.py
@@ -2,9 +2,9 @@ from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'weather_api_wraper.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'weather_api_wrapper.settings')
 
-app = Celery('weather_api_wraper')
+app = Celery('weather_api_wrapper')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
