b3bfe4ee01ed5dd91a2d3d70c3f3fdc1f0368bb0
Leudo Neto <leudoneto1234@gmail.com>
2024-08-28 16:57:21 -0300

    feat/GET for checking progress

--- weather/urls.py
+++ weather/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import CollectWeatherData
+from .views import CollectWeatherData, CheckProgress
 
 urlpatterns = [
     path('collect/', CollectWeatherData.as_view(), name='collect-weather-data'),
+    path('progress/<str:user_defined_id>/', CheckProgress.as_view(), name='check-progress'),
 ]
--- weather/views.py
+++ weather/views.py
@@ -3,9 +3,10 @@ from rest_framework.response import Response
 from rest_framework import status
 from .models import WeatherData
 from .tasks import fetch_weather_data
+from weather_api_wrapper.settings import LEN_CITIES_LIST
 
 class CollectWeatherData(APIView):
-    
+
     def post(self, request):
         user_defined_id = request.data.get('user_defined_id')
         
@@ -19,3 +20,14 @@ class CollectWeatherData(APIView):
 
         return Response({"message": "Data collection initiated", "id": str(id)}, status=status.HTTP_202_ACCEPTED)
 
+
+class CheckProgress(APIView):
+
+    def get(self, request, user_defined_id):
+        if not WeatherData.objects.filter(user_defined_id=user_defined_id).exists():
+            return Response({"error": "ID not found"}, status=status.HTTP_404_NOT_FOUND)
+
+        collected_count = WeatherData.objects.filter(user_defined_id=user_defined_id).count()
+        progress_percentage = (collected_count / LEN_CITIES_LIST) * 100
+
+        return Response({"progress": f"{progress_percentage}%"})
